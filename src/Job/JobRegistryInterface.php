ca67200d8ceec5d6c95e09702d1b54f358a25e29
Andreas Penz <office@dopa.media>
2017-09-28 17:19:15 +0200

    added JobRegistryInterface and UndefinedJobException

---
+++ src/Job/JobRegistryInterface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User: Andreas Penz <office@dopa.media>
+ * Date: 28.09.17
+ */
+
+namespace Dopamedia\PhpBatch\Job;
+
+use Dopamedia\PhpBatch\JobInterface;
+use Dopamedia\PhpBatch\UndefinedJobException;
+
+/**
+ * Interface JobRegistryInterface
+ * @package Dopamedia\PhpBatch\Job
+ */
+interface JobRegistryInterface
+{
+    /**
+     * @param string $jobName
+     * @return JobInterface
+     * @throws UndefinedJobException
+     */
+    public function getJob(string $jobName): JobInterface;
+
+    /**
+     * @return JobInterface[]|array
+     */
+    public function getJobs(): array;
+}
---
+++ src/Job/UndefinedJobException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: Andreas Penz <office@dopa.media>
+ * Date: 28.09.17
+ */
+
+namespace Dopamedia\PhpBatch\Job;
+
+/**
+ * Class UndefinedJobException
+ * @package Dopamedia\PhpBatch\Job
+ */
+class UndefinedJobException extends \DomainException
+{
+
+}

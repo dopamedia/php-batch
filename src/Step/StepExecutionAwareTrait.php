eded534b5703fdf4f612bb3d15f703f483f6952d
Andreas Penz <office@dopa.media>
2017-10-01 10:14:38 +0200

    add StepExecutionAwareTrait

---
+++ src/Step/StepExecutionAwareTrait.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * User: Andreas Penz <office@dopa.media>
+ * Date: 01.10.17
+ */
+
+namespace Dopamedia\PhpBatch\Step;
+
+use Dopamedia\PhpBatch\StepExecutionInterface;
+
+/**
+ * Trait StepExecutionAwareTrait
+ * @package Dopamedia\PhpBatch\Step
+ */
+trait StepExecutionAwareTrait
+{
+    /**
+     * @var null|StepExecutionInterface
+     */
+    protected $stepExecution;
+
+    /**
+     * @param StepExecutionInterface $stepExecution
+     * @return void
+     */
+    public function setStepExecution(StepExecutionInterface $stepExecution): void
+    {
+        $this->stepExecution = $stepExecution;
+    }
+}
